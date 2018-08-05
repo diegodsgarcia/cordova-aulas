5d09b5a6a29fb891aaabfdb4516c1953439e29a4
diegodsgarcia <diego.ds.garcia@gmail.com>
2018-08-04 21:34:23 -0300

    Refactory all css and js

--- iniciando-projeto/www/js/index.js
+++ iniciando-projeto/www/js/index.js
@@ -9,17 +9,18 @@ var logradouro = document.querySelector('.logradouro')
 
 
 form.onsubmit = function(event) {
-    event.preventDefault()
+  event.preventDefault()
 
-    var endereco = {
-        bairro: 'Vila Mariana',
-        cidade: 'São Paulo',
-        estado: 'SP',
-        logradouro: 'Av. Lins de Vasconceles'
-    }
+  var endereco = {
+    bairro: 'Vila Mariana',
+    cidade: 'São Paulo',
+    estado: 'SP',
+    logradouro: 'Av. Lins de Vasconceles'
+  }
 
-    bairro.innerText = 'Rua: ' + endereco.bairro
-    cidade.innerText = 'Cidade: ' + endereco.cidade
-    estado.innerText = 'Estado: ' + endereco.estado
-    logradouro.innerText = 'Logradouro: ' + endereco.logradouro
+  bairro.innerText = 'Rua: ' + endereco.bairro
+  cidade.innerText = 'Cidade: ' + endereco.cidade
+  estado.innerText = 'Estado: ' + endereco.estado
+  logradouro.innerText = 'Logradouro: ' + endereco.logradouro
+  
 }
